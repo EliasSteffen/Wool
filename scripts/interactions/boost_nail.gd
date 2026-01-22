3fbb8486d2db4347e4d5097adf4a62b1a699414a
E. Steffen <es724040@fh-muenster.de>
2026-01-22 23:06:03 +0100

    update

--- scripts/interactions/boost_nail.gd
+++ scripts/interactions/boost_nail.gd
@@ -1,7 +1,7 @@
 class_name BoostNail
 extends BaseNail
 
-@export var boost_multiplier: float = 2.0
+@export var boost_multiplier: float = 4.0
 
 func _setup_interaction() -> void:
 	interaction_name = "BoostNail"
